d640c7cbda0e26d9d661bd35a40ee8ed7b6a21da
bziin <107683186+bziin@users.noreply.github.com>
2023-10-19 15:14:37 +0900

    Add files via upload

---
+++ src/main/java/com/shop/shop/dto/UpdateDto.java
@@ -0,0 +1,36 @@
+package com.shop.shop.dto;
+
+
+import groovy.transform.Sealed;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UpdateDto {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 주소를 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "이름을 입력해주세요.")
+    private String name;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=8, max=16, message="비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+    private String password;
+
+    @NotEmpty(message = "우편번호는 필수 입력 값입니다.")
+    private String zipCode;
+
+    private String streetAddress;
+
+    private String detailAddress;
+}
